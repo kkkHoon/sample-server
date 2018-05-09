4ebed4912363aaa202b295fd9ddcc851a34cdb36
kkkHoon <rlarlgns963@naver.com>
2018-05-09 19:28:23 +0900

    update3

--- server.js
+++ server.js
@@ -31,7 +31,45 @@ app.post('/', function (req, res) {
 
         survey_mdoel.find({}, 'gender age', function(err, result){
             if(err) console.log(err);
-            res.send(result);
+            
+            age_arr = [0, 0, 0, 0];            
+            man_count = 0;
+            woman_count = 0;
+
+            result.forEach(function(doc){
+                if (doc.age >=0 && doc.age < 10)
+                    age_arr[0]++;
+                else if(doc.age < 20)
+                    age_arr[1]++;
+                else if(doc.age < 30)
+                    age_arr[2]++;
+                else
+                    age_arr[3]++;
+
+                if(doc.gender == "Man")
+                    man_count++;
+                else
+                    woman_count++;
+            });
+
+            age_info = {};
+            age_info.chartName = "bar";
+            age_info.data = [ 
+                {"label":"0 ~ 9" , "value":age_arr[0]}, 
+                {"label":"10 ~ 19" , "value":age_arr[1]}, 
+                {"label":"20 ~ 29" , "value":age_arr[2]}, 
+                {"label":"30 ~ " , "value":age_arr[3]} 
+            ];
+
+            gender_info = {};
+            gender_info.chartName = "Donught";
+            gender_info.data = [
+                {"label":"Man", "value":man_count},
+                {"label":"Woman", "value":woman_count}
+            ];
+
+            result_infos = [age_info, gender_info];
+            res.send(result_infos);
         })
     });
 });
